868aaeec0ff6c04f194c9fd86f74d364a6294424
Nathan Malenfant <njm94@drexel.edu>
2018-11-22 12:11:52 -0500

    Updated Animations
    Simplified animations and added direction idle animations

--- 2-D Top-Down Foundation/Assets/Scripts/PlayerController.cs
+++ 2-D Top-Down Foundation/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : MonoBehaviour {
 
     private Animator animator;
 
+    private Vector2 lastMove; //Use Vector 2 for (x,y); not Vector 3 b/c it's (x,y,z)
+    private bool playerMoving;
+
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
@@ -16,20 +19,29 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
+        playerMoving = false;
+
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f) //GetAxisRaw takes the input of that very second
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
         }
 
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f) //GetAxisRaw takes the input of that very second
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0, Input.GetAxisRaw("Vertical"));
         }
 
         animator.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         animator.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
-        //In order to not make the animations take time to activate (i.e. the player starts moving, but is still in the idle animation for a little while),
-        //click on the transition, uncheck "Has Exit Time," uncheck "Fixed Duration," and set "Transition Duration" to 0. Supposedly, "Transition Duration
-        //is what causes this the most.
+        animator.SetBool("PlayerMoving", playerMoving);
+        animator.SetFloat("LastMoveX", lastMove.x);
+        animator.SetFloat("LastMoveY", lastMove.y);
+        /*In order to not make the animations take time to activate (i.e. the player starts moving, but is still in the idle animation for a little while),
+        click on the transition, uncheck "Has Exit Time," uncheck "Fixed Duration," and set "Transition Duration" to 0. Supposedly, "Transition Duration
+        is what causes this the most.*/
     }
 }
